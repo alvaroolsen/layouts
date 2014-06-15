58ab2bb935ed169fa7cd5cb6e50f26a64be02605
Al Olsen <alvaro.olsen@gmail.com>
2014-06-14 21:44:18 -0400

    Added a Scroll To Animation and a Dynamically generated width for a slider

--- scripts/mechanix.js
+++ scripts/mechanix.js
@@ -5,9 +5,28 @@ $(document).ready(function(){
 
 		$('html, body').stop().animate({
 	        scrollTop: $($anchor.attr('href')).offset().top
-	    }, 200);
+	    }, 1000);
 	    
 	    event.preventDefault();
 
 	});
+
+
+	var newSlidesWidth = $(".slide").width();
+	var totalSlides = $(".slider .slides").children();
+
+	console.log(totalSlides.length)
+
+	for(var i = 0; i < totalSlides.length; i++){
+
+		console.log($(totalSlides[i]).width())
+		console.log(i)
+		newSlidesWidth = newSlidesWidth + $(totalSlides[i]).width();
+	}
+
+	console.log(newSlidesWidth);
+
+	$(".slides").css({width:(newSlidesWidth)+"px"})
+
+	
 });
